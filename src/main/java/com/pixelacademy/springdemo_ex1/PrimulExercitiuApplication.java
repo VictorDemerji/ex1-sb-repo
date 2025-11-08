8cc9f44ede4241861db9c106da66efb544b92e56
VictorDemerji <demerji.v1@gmail.com>
2025-11-08 15:26:17 +0200

    Initial commit

---
+++ src/main/java/com/pixelacademy/springdemo_ex1/PrimulExercitiuApplication.java
@@ -0,0 +1,13 @@
+package com.pixelacademy.springdemo_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimulExercitiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimulExercitiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelacademy/springdemo_ex1/PrimulExercitiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelacademy.springdemo_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimulExercitiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
